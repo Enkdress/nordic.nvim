180f119b71af68219141d4f7428989ea1f097468
Sergio Correa <felizjanuk12@gmail.com>
2023-08-04 22:02:26 -0500

    add some highlights to neorg

---
+++ lua/nordic/groups/integrations/neorg.lua
@@ -0,0 +1,35 @@
+local C = require 'nordic.colors'
+local O = require('nordic.config').options
+
+return {
+    ["@neorg.headings.1.title"] = { fg = C.orange.base },
+    ["@neorg.headings.2.title"] = { fg = C.orange.base },
+    ["@neorg.headings.3.title"] = { fg = C.orange.base },
+    ["@neorg.headings.4.title"] = { fg = C.orange.base },
+    ["@neorg.headings.5.title"] = { fg = C.orange.base },
+    ["@neorg.headings.6.title"] = { fg = C.orange.base },
+    ["@neorg.headings.1.prefix"] = { fg = C.gray1 },
+    ["@neorg.headings.2.prefix"] = { fg = C.gray1 },
+    ["@neorg.headings.3.prefix"] = { fg = C.gray1 },
+    ["@neorg.headings.4.prefix"] = { fg = C.gray1 },
+    ["@neorg.headings.5.prefix"] = { fg = C.gray1 },
+    ["@neorg.headings.6.prefix"] = { fg = C.gray1 },
+
+    ["@neorg.links.location.heading.1"] = { fg = C.orange.base },
+    ["@neorg.links.location.heading.2"] = { fg = C.orange.base },
+    ["@neorg.links.location.heading.3"] = { fg = C.orange.base },
+    ["@neorg.links.location.heading.4"] = { fg = C.orange.base },
+    ["@neorg.links.location.heading.5"] = { fg = C.orange.base },
+    ["@neorg.links.location.heading.6"] = { fg = C.orange.base },
+    ["@neorg.links.location.heading.1.prefix"] = { fg = C.gray1 },
+    ["@neorg.links.location.heading.2.prefix"] = { fg = C.gray1 },
+    ["@neorg.links.location.heading.3.prefix"] = { fg = C.gray1 },
+    ["@neorg.links.location.heading.4.prefix"] = { fg = C.gray1 },
+    ["@neorg.links.location.heading.5.prefix"] = { fg = C.gray1 },
+    ["@neorg.links.location.heading.6.prefix"] = { fg = C.gray1 },
+
+
+    ["@neorg.links.location.url"] = { fg = C.magenta.base },
+
+    ["@neorg.markup.italic.norg"] = { fg = C.white1 },
+}
